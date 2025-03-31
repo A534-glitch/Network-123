72c075ffd9ee032b373cf3f87b30451939a7ba49
A534-glitch <sadharsh306@gmail.com>
2025-03-31 07:21:37 +0530

    Add files via upload

---
+++ Menudriven.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+while true
+do
+clear
+echo "Menu:"
+echo "1. List current directory"
+echo "2. Print working directory"
+echo "3. Display date"
+echo "4. Display users logged in"
+echo "5. Exit"
+read -p "Enter your choice: " choice
+case $choice in
+1)
+ls -l
+read -p "Press enter to continue"
+;;
+2)
+pwd
+read -p "Press enter to continue"
+;;
+3)
+date
+read -p "Press enter to continue"
+;;
+who
+read -p "Press enter to continue"
+;;
+exit 0
+;;
+echo "Invalid choice. Press enter to try again" 
+read
+;;
+esac
+done
